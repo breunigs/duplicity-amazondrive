ba1691520418953e81235007a36b774b30b75d79
Max Ehrlich <max.ehr@gmail.com>
2016-01-02 11:31:15 -0500

    Moved file deletion to finally block
    Wrapping acd_cli subprocess call ensures that the temporary file is removed if acd_cli has a non-zero exit code

--- amazonclouddrivebackend.py
+++ amazonclouddrivebackend.py
@@ -71,11 +71,6 @@ class ACDBackend(duplicity.backend.Backend):
         deleteFile = False
         if(source_path.name != local_real_duplicity_file):
             try:
-                if os.path.exists(local_real_duplicity_file):
-                    try:
-                        os.remove(local_real_duplicity_file)
-                    except OSError, e:
-                        log.FatalError("Unable to remove file %s" % e)                    
                 os.symlink(source_path.name, local_real_duplicity_file)
                 deleteFile = True
             except IOError, e:
@@ -83,13 +78,15 @@ class ACDBackend(duplicity.backend.Backend):
 
         commandline = self.acd_cmd + " upload --force --overwrite '%s' '%s'" % \
             (local_real_duplicity_file, remote_path)
-        l = self.subprocess_popen(commandline)
 
-        if (deleteFile):
-            try:
-                os.remove(local_real_duplicity_file)
-            except OSError, e:
-                log.FatalError("Unable to remove file %s" % e)
+        try:
+            l = self.subprocess_popen(commandline)
+        finally:
+            if (deleteFile):
+                try:
+                    os.remove(local_real_duplicity_file)
+                except OSError, e:
+                    log.FatalError("Unable to remove file %s" % e)
 
     def _get(self, remote_filename, local_path):
         """Get remote filename, saving it to local_path"""
