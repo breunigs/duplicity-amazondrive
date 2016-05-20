fd29f5e8e5dc5c0b6b3521c8d7aa9270cc48efeb
Stefan Breunig <stefan-github@yrden.de>
2016-05-20 22:18:54 +0200

    more cleanup

--- amazonclouddrivebackend.py
+++ amazonclouddrivebackend.py
@@ -25,10 +25,16 @@ import sys
 from io import DEFAULT_BUFFER_SIZE
 
 import duplicity.backend
+from duplicity.errors import BackendException
 from duplicity import globals
 from duplicity import log
 
 class ACDBackend(duplicity.backend.Backend):
+    """
+    Backend for AmazonCloudDrive. It communicates directly with ACD using their
+    RESTful API and does not rely on externally setup software (like acd_cli).
+    """
+
     OAUTH_TOKEN_PATH = os.path.expanduser('~/.duplicity_acd_oauthtoken.json')
 
     OAUTH_AUTHORIZE_URL = 'https://www.amazon.com/ap/oa'
@@ -49,60 +55,54 @@ class ACDBackend(duplicity.backend.Backend):
         self.content_url = 'https://content-na.drive.amazonaws.com/cdproxy/'
 
         self.names_to_ids = {}
-        self.logical_path_id = None
-        self.logical_path = parsed_url.path.lstrip('/')
-
-        if self.logical_path == "":
-            raise BackendException((
-                'You did not specify a path. '
-                'Please specify a path, e.g. acd://duplicity_backups'))
+        self.backup_target_id = None
+        self.backup_target = parsed_url.path.lstrip('/')
 
         if globals.volsize > (10 * 1024 * 1024 * 1024):
             # https://forums.developer.amazon.com/questions/22713/file-size-limits.html
             # https://forums.developer.amazon.com/questions/22038/support-for-chunked-transfer-encoding.html
-            raise BackendException((
+            raise BackendException(
                 'Your --volsize is bigger than 10 GiB, which is the maximum '
-                'file size on ACD that does not require work arounds.'))
+                'file size on ACD that does not require work arounds.')
 
-        self.import_dependencies()
-        self.initialize_oauth2_session()
-        self.resolve_logical_path()
-
-    def import_dependencies(self):
         try:
-            # On debian (and derivatives), get these dependencies using:
-            # apt-get install python-requests python-requests-oauthlib
-            # On fedora (and derivatives), get these dependencies using:
-            # yum install python-requests python-requests-oauthlib
             global requests
             global OAuth2Session
             import requests
             from requests_oauthlib import OAuth2Session
         except ImportError:
-            raise BackendException((
+            raise BackendException(
                 'ACD backend requires python-requests and '
-                'python-requests-oauthlib to be installed. Please install '
-                'them and try again.'))
+                'python-requests-oauthlib to be installed.\n\n'
+                'For Debian and derivates use:\n'
+                '  apt-get install python-requests python-requests-oauthlib\n'
+                'For Fedora and derivates use:\n'
+                '  yum install python-requests python-requests-oauthlib')
+
+        self.initialize_oauth2_session()
+        self.resolve_backup_target()
 
     def initialize_oauth2_session(self):
+        """Setup or refresh oauth2 session with ACD"""
+
         def token_updater(token):
+            """Stores oauth2 token on disk"""
             try:
                 with open(self.OAUTH_TOKEN_PATH, 'w') as f:
                     json.dump(token, f)
-            except Exception as e:
-                log.Error(('Could not save the OAuth2 token to %s. '
-                           'This means you may need to do the OAuth2 '
-                           'authorization process again soon. '
-                           'Original error: %s' % (
-                               self.OAUTH_TOKEN_PATH, e)))
+            except Exception as err:
+                log.Error('Could not save the OAuth2 token to %s. This means '
+                          'you may need to do the OAuth2 authorization '
+                          'process again soon. Original error: %s' % (
+                              self.OAUTH_TOKEN_PATH, err))
 
         token = None
         try:
             with open(self.OAUTH_TOKEN_PATH) as f:
                 token = json.load(f)
-        except IOError as e:
-            log.Error(('Could not load OAuth2 token. '
-                       'Trying to create a new one. (original error: %s)' % e))
+        except IOError as err:
+            log.Notice('Could not load OAuth2 token. '
+                       'Trying to create a new one. (original error: %s)' % err)
 
         self.http_client = OAuth2Session(
             self.CLIENT_ID,
@@ -119,77 +119,83 @@ class ACDBackend(duplicity.backend.Backend):
         if token is not None:
             self.http_client.refresh_token(self.OAUTH_TOKEN_URL)
 
-        user_endpoint_response = self.http_client.get(self.metadata_url
-            + 'account/endpoint')
-        if user_endpoint_response.status_code != requests.codes.ok:
+        endpoints_response = self.http_client.get(self.metadata_url
+                                                  + 'account/endpoint')
+        if endpoints_response.status_code != requests.codes.ok:
             token = None
 
         if token is None:
             if not sys.stdout.isatty() or not sys.stdin.isatty():
-                log.FatalError(('The OAuth2 token could not be loaded from %s '
-                                'and you are not running duplicity '
-                                'interactively, so duplicity cannot possibly '
-                                'access ACD.' % self.OAUTH_TOKEN_PATH))
-            authorization_url, state = self.http_client.authorization_url(
+                log.FatalError('The OAuth2 token could not be loaded from %s '
+                               'and you are not running duplicity '
+                               'interactively, so duplicity cannot possibly '
+                               'access ACD.' % self.OAUTH_TOKEN_PATH)
+            authorization_url, _ = self.http_client.authorization_url(
                 self.OAUTH_AUTHORIZE_URL)
 
             print ''
-            print ('In order to authorize duplicity to access your ACD, '
-                   'please open the following URL in a browser and copy '
-                   'the URL of the blank page the dialog leads to: %s'
-                    % authorization_url)
+            print ('In order to authorize duplicity to access your ACD, please '
+                   'open the following URL in a browser and copy the URL of '
+                   'the blank page the dialog leads to: %s' % authorization_url)
             print ''
 
             redirected_to = (raw_input('URL of the blank page: ')
-                .replace('http://', 'https://', 1))
+                             .replace('http://', 'https://', 1))
 
             token = self.http_client.fetch_token(
                 self.OAUTH_TOKEN_URL,
                 client_secret=self.CLIENT_SECRET,
                 authorization_response=redirected_to)
 
-            user_endpoint_response = self.http_client.get(self.metadata_url
-                 + 'account/endpoint')
-            user_endpoint_response.raise_for_status()
+            endpoints_response = self.http_client.get(self.metadata_url
+                                                      + 'account/endpoint')
+            endpoints_response.raise_for_status()
             token_updater(token)
 
-        urls = user_endpoint_response.json()
+        urls = endpoints_response.json()
         if 'metadataUrl' not in urls or 'contentUrl' not in urls:
-            log.FatalError('Could not retrieve endpoint URLs for this account.')
+            log.FatalError('Could not retrieve endpoint URLs for this account')
         self.metadata_url = urls['metadataUrl']
         self.content_url = urls['contentUrl']
 
-    def resolve_logical_path(self):
+    def resolve_backup_target(self):
+        """Resolve node id for remote backup target folder"""
+
         folders_response = self.http_client.get(self.metadata_url + 'nodes?filters=kind:FOLDER')
         folders = folders_response.json()['data']
 
-        root_node = (f for f in folders if f.get('isRoot') == True).next()
+        root_node = (f for f in folders if f.get('isRoot')).next()
         parent_node_id = root_node['id']
 
-        for component in [x for x in self.logical_path.split('/') if x]:
+        for component in [x for x in self.backup_target.split('/') if x]:
             candidates = [f for f in folders if f.get('name') == component and
-                parent_node_id in f['parents']]
+                          parent_node_id in f['parents']]
 
             if len(candidates) >= 2:
-                log.FatalError(('There are multiple folders with the same name '
-                                'in the same parent. ParentID: %s FolderName: '
-                                '%s' % (parent_node_id, component)))
+                log.FatalError('There are multiple folders with the same name '
+                               'below one parent.\nParentID: %s\nFolderName: '
+                               '%s' % (parent_node_id, component))
             elif len(candidates) == 1:
                 parent_node_id = candidates[0]['id']
             else:
+                log.Debug('Folder %s does not exist yet. Creating.' % component)
                 parent_node_id = self.mkdir(parent_node_id, component)
 
-        log.Debug("Backup Folder has node id: %s" % parent_node_id)
-        self.logical_path_id = parent_node_id
+        log.Debug("Backup target folder has id: %s" % parent_node_id)
+        self.backup_target_id = parent_node_id
 
     def get_file_id(self, remote_filename):
-        if (remote_filename not in self.names_to_ids):
+        """Find id of remote file in backup target folder"""
+
+        if remote_filename not in self.names_to_ids:
             self._list()
 
         return self.names_to_ids.get(remote_filename)
 
     def mkdir(self, parent_node_id, folder_name):
-        data = { 'name': folder_name, 'parents': [parent_node_id], 'kind' : 'FOLDER' }
+        """Create a new folder as a child of a parent node"""
+
+        data = {'name': folder_name, 'parents': [parent_node_id], 'kind' : 'FOLDER'}
         response = self.http_client.post(
             self.metadata_url + 'nodes',
             data=json.dumps(data))
@@ -197,6 +203,8 @@ class ACDBackend(duplicity.backend.Backend):
         return response.json()['id']
 
     def multipart_stream(self, metadata, source_path):
+        """Generator for multipart/form-data file upload from source file"""
+
         boundary = self.MULTIPART_BOUNDARY
 
         yield str.encode('--%s\r\nContent-Disposition: form-data; '
@@ -218,6 +226,8 @@ class ACDBackend(duplicity.backend.Backend):
                          'multipart/form-data; boundary=%s' % boundary)
 
     def _put(self, source_path, remote_filename):
+        """Upload a local file to ACD"""
+
         quota = self.http_client.get(self.metadata_url + 'account/quota')
         quota.raise_for_status()
         available = quota.json()['available']
@@ -225,14 +235,14 @@ class ACDBackend(duplicity.backend.Backend):
         source_size = os.path.getsize(source_path.name)
 
         if source_size > available:
-            raise BackendException((
+            raise BackendException(
                 'Out of space: trying to store "%s" (%d bytes), but only '
                 '%d bytes available on ACD.' % (
-                    source_path.name, source_size, available)))
+                    source_path.name, source_size, available))
 
-        metadata = { 'name': remote_filename, 'kind': 'FILE', 'parents': [self.logical_path_id] }
-        headers = { 'Content-Type': 'multipart/form-data; boundary=%s'
-                                                     % self.MULTIPART_BOUNDARY}
+        metadata = {'name': remote_filename, 'kind': 'FILE', 'parents': [self.backup_target_id]}
+        headers = {'Content-Type': 'multipart/form-data; boundary=%s'
+                                   % self.MULTIPART_BOUNDARY}
         data = self.multipart_stream(metadata, source_path)
 
         response = self.http_client.post(
@@ -247,22 +257,26 @@ class ACDBackend(duplicity.backend.Backend):
         self.names_to_ids[response.json()['name']] = response.json()['id']
 
     def _get(self, remote_filename, local_path):
-        with local_path.open('wb') as f:
+        """Download file from ACD"""
+
+        with local_path.open('wb') as local_file:
             file_id = self.get_file_id(remote_filename)
             if file_id is None:
-                raise BackendException((
-                    'File "%s" cannot be downloaded: it does not exist' % (
-                        remote_filename)))
+                raise BackendException(
+                    'File "%s" cannot be downloaded: it does not exist' %
+                    remote_filename)
 
             response = self.http_client.get(
                 self.content_url + '/nodes/' + file_id + '/content', stream=True)
             response.raise_for_status()
             for chunk in response.iter_content(chunk_size=DEFAULT_BUFFER_SIZE):
                 if chunk:
-                    f.write(chunk)
-            f.flush()
+                    local_file.write(chunk)
+            local_file.flush()
 
     def _query(self, remote_filename):
+        """Retrieve file size info from ACD"""
+
         file_id = self.get_file_id(remote_filename)
         if file_id is None:
             return {'size': -1}
@@ -272,23 +286,29 @@ class ACDBackend(duplicity.backend.Backend):
         return {'size': response.json()['contentProperties']['size']}
 
     def _list(self):
-        children_response = self.http_client.get(self.metadata_url + 'nodes/' + self.logical_path_id + '/children')
+        """List files in ACD backup folder"""
+
+        children_response = self.http_client.get(
+            self.metadata_url + 'nodes/' + self.backup_target_id + '/children')
         children = children_response.json()['data']
 
         files = [f for f in children if f['kind'] == 'FILE']
 
-        self.names_to_ids = { f['name']: f['id'] for f in files }
+        self.names_to_ids = {f['name']: f['id'] for f in files}
 
         return self.names_to_ids.keys()
 
 
     def _delete(self, remote_filename):
+        """Delete file from ACD"""
+
         file_id = self.get_file_id(remote_filename)
         if file_id is None:
-            raise BackendException((
+            raise BackendException(
                 'File "%s" cannot be deleted: it does not exist' % (
-                    remote_filename)))
+                    remote_filename))
         response = self.http_client.put(self.metadata_url + 'trash/' + file_id)
         response.raise_for_status()
+        del self.names_to_ids[remote_filename]
 
 duplicity.backend.register_backend('acd', ACDBackend)
